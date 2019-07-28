ffed3b28a20325c7f572512eadac86633cd886c3
kmtc44 <kairmor@yahoo.com>
2019-07-28 23:51:07 +0000

    api key

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const baseSite = 'https://ge-state-ept.herokuapp.com'
-export const apiKey = 'AIzaSyC9EFyrUQQE1rWQflxaLM03gntsILbouzI'
+export const apiKey = 'AIzaSyDaQA7IpV1vb76eTUxYC1tIPE5-_sEDTPY'
