53ad6594953de022008f6c2c2f5cfe561d1d08e4
kmtc44 <kairmor@yahoo.com>
2019-07-28 21:56:19 +0000

    config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const baseSite = 'http://localhost:8000'
-export const baseSite = 'https://ge-state-ept.herokuapp.com/'
+export const baseSite = 'https://ge-state-ept.herokuapp.com'
 export const apiKey = 'AIzaSyC9EFyrUQQE1rWQflxaLM03gntsILbouzI'
